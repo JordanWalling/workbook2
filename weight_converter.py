51f0fd5c9eb36db5a7813b843ab0bea02370eb40
jordanwalling <jordanwalling1991@gmail.com>
2022-07-10 16:17:29 +1000

    fix weight converter function

--- weight_converter.py
+++ weight_converter.py
@@ -1,10 +1,15 @@
 def convert_weight():
-    prompt = input("What would you like to convert? \n1. Pounds to Kilograms \n2. Kilograms")
-
+    prompt = False
+    
+    print("What would you like to convert? \n1. Pounds to Kilograms \n2. Kilograms")
+    prompt = input("Please answer either '1' or '2'")
     if prompt == "1":
         num = input("Please enter your weight in Pounds: ")
-        print('This is your weight in kilograms: ')
+        print(f'This is your weight in kilograms: {num*0.45359237} ')
 
     elif prompt == "2":
         num = input("Please enter your weight in Kilograms: ")
-        print("This is your weight in Pounds: ")
+        print(f"This is your weight in Pounds: {num*2.2} ")
+
+    else: 
+        print("Invalid option, please enter '1' or '2'")
