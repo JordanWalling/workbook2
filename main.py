de77f1c77df6dccec76eff73d5a7d92582505db5
jordanwalling <jordanwalling1991@gmail.com>
2022-07-10 15:19:52 +1000

    create prompt for main page

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ print('Welcome to the Health App!')
 print('Please select an option: ')
 prompt = input('1.BMI \n2.Weight Converter \n3.Calories needed \n')
 
+if prompt == "1":
+    print("BMI")
+elif prompt == "2":
+    print("Weight Converter")
+elif prompt == "3":
+    print("Calories needed")
+else:
+    print("Invalid input")
