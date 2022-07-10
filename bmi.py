367d1d11ecd6977938a6f89d5689a9d903c3bb25
jordanwalling <jordanwalling1991@gmail.com>
2022-07-10 15:31:24 +1000

    create bmi ranges

--- bmi.py
+++ bmi.py
@@ -3,3 +3,13 @@ def calculate_bmi():
     height = input("Please enter your height in metres: ")
     weight = input("Please enter your weight in kilograms: ")
     bmi = weight/height**2
+    print(f"Your BMI is: {bmi}")
+
+    if bmi <18.50:
+        print("You are Underweight")
+    elif bmi >=18.50 and bmi <= 24.99:
+        print("You are in the health range bracket!")
+    elif bmi >= 25.00 and bmi <= 29.99:
+        print("According to your BMI, you are Overweight.")
+    else:
+        print("According to the BMI, You are Obese.")
