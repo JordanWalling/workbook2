372b468adf97a979b875ff8446217b716c38a361
jordanwalling <jordanwalling1991@gmail.com>
2022-07-10 15:37:50 +1000

    fix input for height/weight for bmi calculator

--- bmi.py
+++ bmi.py
@@ -1,7 +1,7 @@
 def calculate_bmi():
     print("To calculate your Body Mass Index or BMI, your height and weight is needed")
-    height = input("Please enter your height in metres: ")
-    weight = input("Please enter your weight in kilograms: ")
+    height = float(input("Please enter your height in metres: "))
+    weight = float(input("Please enter your weight in kilograms: "))
     bmi = weight/height**2
     print(f"Your BMI is: {bmi}")
 
