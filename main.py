f3424fd67f50afa67cd5c37223148a27cfd9cf10
jordanwalling <jordanwalling1991@gmail.com>
2022-07-10 15:34:29 +1000

    add bmi calculator to main page

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from bmi import calculate_bmi
+
 print('Welcome to the Health App!')
 print('Please select an option: ')
 prompt = input('1.BMI \n2.Weight Converter \n3.Calories needed \n')
 
 if prompt == "1":
-    print("BMI")
+    calculate_bmi()
 elif prompt == "2":
     print("Weight Converter")
 elif prompt == "3":
