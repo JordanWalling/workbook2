d411eb316056b39baa2828963f1c4ac2d6058edf
jordanwalling <jordanwalling1991@gmail.com>
2022-07-10 15:03:10 +1000

    add main page

---
+++ stopwatch.py
@@ -0,0 +1,18 @@
+import time
+
+def print_time(total_seconds):
+    total_mins = total_seconds / 60
+    seconds = int(total_seconds % 60)
+    hours = int(total_mins / 60)
+    mins = int(total_mins % 60)
+    print('Time spent: {}h: {}m: {}s'.format(hours, mins, seconds))
+
+input('Press Enter/Return key to start stopwatch: ')
+start_time = time.time()
+
+print('counting time...')
+
+input('Press Enter/Return key to stop the stopwatch: ')
+stop_time = time.time()
+
+print_time(stop_time - start_time)
